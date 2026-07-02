e36d93e3344e571283e1e607e7049c00ddbea0b9
Cointrader <cointrader1985@gmail.com>
2026-07-02 08:56:04 +0100

    Create Shape.cpp circle
    circle

---
+++ Shape.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int generateShape() {
+    return std::rand() % 3; // 0 circle, 1 triangle, 2 square
+}
+
+int main() {
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+    int score = 0;
+    int rounds = 8;
+
+    std::cout << "=== Shape Quest Arena ===\n";
+
+    for (int i = 1; i <= rounds; i++) {
+        int shape = generateShape();
+        int guess;
+
+        std::cout << "\nRound " << i << "\n";
+        std::cout << "Guess shape (0=circle, 1=triangle, 2=square): ";
+        std::cin >> guess;
+
+        if (guess == shape) {
+            std::cout << "Correct shape!\n";
+            score += 10;
+        } else {
+            std::cout << "Wrong choice.\n";
+            score += 3;
+        }
+
+        if (shape == 0)
+            std::cout << "A smooth circle appears.\n";
+        else if (shape == 1)
+            std::cout <
